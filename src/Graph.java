92a795a5824e19d37f2819ceaf042cb475b71310
leehyeonkeun <montingy@naver.com>
2021-11-21 17:36:19 +0700

    factorial

--- src/Graph.java
+++ src/Graph.java
@@ -1,38 +1,38 @@
 import java.util.LinkedList;
 
 public class Graph {
-    private LinkedList<Integer>[] adj; // 인접 리스트 생성
-    private int v; // vertex - 정점
-
-    // 그래프 생성 및 초기화
-    Graph(int v){
-        this.v = v; // 정점 개수 초기화
-        // visit = new boolean[v];
-        adj = new LinkedList[v+1]; // v로 시작하면 0부터 시작하니깐 보기 쉽게 1부터함. 0은 공백으로 둠
-        // 인접리스트 초기화 작업
-        for(int i=0; i<v+1; i++){
-            adj[i] = new LinkedList();
-        }
-    }
-
-    // 그래프 인자끼리 연결하기
-    void addEdge(int v1, int v2){
-        adj[v1].add(v2);
-        adj[v2].add(v1);
-    }
-
-    public static void main(String[] args) {
-        Graph dfs_g = new Graph(5);
-
-        dfs_g.addEdge(1,2);
-        dfs_g.addEdge(1,3);
-        dfs_g.addEdge(3,4);
-        dfs_g.addEdge(3,5);
-
-        // 인자값 1부터 출
-        for(int i=1; i<dfs_g.adj.length; i++){
-            System.out.println(i+":"+dfs_g.adj[i]);
-        }
-    }
+//    private LinkedList<Integer>[] adj; // 인접 리스트 생성
+//    private int v; // vertex - 정점
+//
+//    // 그래프 생성 및 초기화
+//    Graph(int v){
+//        this.v = v; // 정점 개수 초기화
+//        // visit = new boolean[v];
+//        adj = new LinkedList[v+1]; // v로 시작하면 0부터 시작하니깐 보기 쉽게 1부터함. 0은 공백으로 둠
+//        // 인접리스트 초기화 작업
+//        for(int i=0; i<v+1; i++){
+//            adj[i] = new LinkedList();
+//        }
+//    }
+//
+//    // 그래프 인자끼리 연결하기
+//    void addEdge(int v1, int v2){
+//        adj[v1].add(v2);
+//        adj[v2].add(v1);
+//    }
+//
+//    public static void main(String[] args) {
+//        Graph dfs_g = new Graph(5);
+//
+//        dfs_g.addEdge(1,2);
+//        dfs_g.addEdge(1,3);
+//        dfs_g.addEdge(3,4);
+//        dfs_g.addEdge(3,5);
+//
+//        // 인자값 1부터 출
+//        for(int i=1; i<dfs_g.adj.length; i++){
+//            System.out.println(i+":"+dfs_g.adj[i]);
+//        }
+//    }
 
 }
---
+++ src/sumBiggest.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class sumBiggest {
+//    // 재귀함수를 쓴다니;; 나에게 생소한 방법이네
+//// 가장 큰 두 수의 합 구하기
+//    static int[] Nums = {1,2,3,4,5};
+//    static int N = 5;
+//
+//    // pos 현재 위치
+//    // cnt 선택된 개수
+//    static int solve(int pos, int cnt, int val){
+//
+//        System.out.println(pos + " / " + cnt + " / " + val);
+//
+//        if(cnt == 2) {
+//            System.out.println("ret val = " + val);
+//            return val;
+//        }
+//        if(pos == N) {
+//            System.out.println("End");
+//            return -1;
+//        }
+//
+//        int ret = 0;
+//
+//        ret = Math.max(ret, solve(pos+1, cnt+1, val+Nums[pos]));
+//        System.out.println("first ret = "+ret);
+//        ret = Math.max(ret, solve(pos+1, cnt, val));
+//        System.out.println("second ret = "+ret);
+//
+//        return ret;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(solve(0,0,0));
+//    }
+
+}
